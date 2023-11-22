cae77a2d6224e9a5a30f36d81bf7d5941ba17202
Rohant <agrawalrohant@gmail.com>
2023-11-22 08:35:39 +0530

    setup

---
+++ Consistent Hashing.js
@@ -0,0 +1,7 @@
+module.exports = {
+  //param A : array of strings
+  //param B : array of strings
+  //param C : array of integers
+  //return a array of integers
+  solve: function (A, B, C) {},
+};
---
+++ main.js
@@ -0,0 +1,7 @@
+var filename = "./Consistent Hashing.js"; // Update the file name to debug here
+
+var scalerModule = require(filename);
+
+let reuslt = scalerModule.solve("acafdebae", 2);
+
+console.log("Result : " + reuslt);
